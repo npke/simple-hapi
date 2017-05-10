6307ad2ee089c858439b648426d5838549b9b4c7
Ke Nguyen <ke.nguyen@2359media.com>
2017-05-10 22:46:57 +0700

    init server

---
+++ server.js
@@ -0,0 +1,15 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({port: 8080});
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply({message: 'Hello, World!', info: 'Powered by Hapi framework'});
+  }
+});
+
+server.start((err) => {
+  return err ? console(err) : console.log('Server running at port 8080');
+});
